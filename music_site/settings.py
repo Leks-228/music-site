7082724280c849a230fc686630574fa7007b324a
Leks-228 <sasaonackij789@gmail.com>
2026-06-28 23:13:55 +0400

    fix database config

--- music_site/settings.py
+++ music_site/settings.py
@@ -16,7 +16,7 @@ DEBUG = os.environ.get("DEBUG", "False") == "True"
 
 ALLOWED_HOSTS = os.environ.get(
     "ALLOWED_HOSTS",
-    "127.0.0.1,localhost"
+    "127.0.0.1,localhost,.onrender.com"
 ).split(",")
 
 # -------------------
@@ -36,7 +36,6 @@ INSTALLED_APPS = [
     'crispy_bootstrap5',
 
     'whitenoise.runserver_nostatic',
-
 ]
 
 # -------------------
@@ -77,33 +76,23 @@ WSGI_APPLICATION = 'music_site.wsgi.application'
 # -------------------
 # DATABASE (FIXED)
 # -------------------
-db_url = os.environ.get("DATABASE_URL", "")
+DATABASE_URL = os.environ.get("DATABASE_URL")
 
-if isinstance(db_url, bytes):
-    db_url = db_url.decode("utf-8")
-
-if db_url:
-    db_url = db_url.replace("postgres://", "postgresql://")
-
-import os
-import dj_database_url
-
-db_url = os.environ.get("DATABASE_URL")
-
-if not db_url:
-    raise Exception("DATABASE_URL is NOT set in environment")
-
-if isinstance(db_url, bytes):
-    db_url = db_url.decode("utf-8")
-
-db_url = db_url.strip()
-
-if db_url.startswith("postgres://"):
-    db_url = db_url.replace("postgres://", "postgresql://")
-
-DATABASES = {
-    "default": dj_database_url.parse(db_url)
-}
+if DATABASE_URL:
+    DATABASES = {
+        "default": dj_database_url.parse(
+            DATABASE_URL,
+            conn_max_age=600,
+            ssl_require=True
+        )
+    }
+else:
+    DATABASES = {
+        "default": {
+            "ENGINE": "django.db.backends.sqlite3",
+            "NAME": BASE_DIR / "db.sqlite3",
+        }
+    }
 
 # -------------------
 # PASSWORDS
@@ -132,11 +121,10 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 # -------------------
-# MEDIA (CLOUDINARY ONLY)
+# MEDIA (CLOUDINARY)
 # -------------------
 DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.MediaCloudinaryStorage'
 
-# ❗ ВАЖНО: убираем локальный media полностью
 MEDIA_URL = ''
 MEDIA_ROOT = ''
 
